9857c5cf7bfe13b1cd95e597ddb604c0e2137a8e
Charles Deiniel D. Villanueva <charlesdvillanueva16@gmail.com>
2025-05-24 14:05:45 +0800

    Re-upload of initial files

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
+let passwordLength = 15
 
-function generate() {
-    for (i = 1; i < 16; i++){
-        
-    }
-    randomCharacterIndex = Math.floor(Math.random() * characters.length)
-    console.log(characters[randomCharacterIndex])
-    
+let passwordOne = document.getElementById("password-one-el")
+let passwordTwo = document.getElementById("password-two-el")
+
+
+function getRandomCharacter() {
+    let randomIndex = Math.floor( Math.random() * characters.length )
+    return characters[randomIndex] 
 }
 
+function randomizePassword() {
+    let password = ""
+    for (let i = 0; i < passwordLength; i++){
+        password += getRandomCharacter()
+    }
+    return password
+}
+
+function generatePassword() {
+    passwordOne.textContent = randomizePassword()
+    passwordTwo.textContent = randomizePassword()
+}
